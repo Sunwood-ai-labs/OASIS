929c4288e71760f6056d3e4cc4612cebebff9906
Maki <sunwood.ai.labs@gmail.com>
2024-06-26 22:10:51 +0900

    🔼 [feat] パッケージバージョンを0.2.1に更新
    - setup.pyのバージョン番号を0.2.0から0.2.1に変更しました。この変更により、小さな改善やバグ修正がパッケージに反映されます。

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="oasis-article",
-    version="0.2.0",
+    version="0.2.1",
     author="Sunwood-ai-labs",
     author_email="sunwood.ai.labs@gmail.com",
     description="WordPressへの投稿を自動化するパッケージ",
