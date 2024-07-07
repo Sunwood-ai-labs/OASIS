9379932e88e27df8b4008d5544e7bcde633fe77e
Maki <sunwood.ai.labs@gmail.com>
2024-07-07 14:26:11 +0900

    🧹 [chore] 未使用のリンクプレビュー処理コードを削除
    - MarkdownからHTMLへの変換処理を行うクラスから、使用されていないリンクプレビュー関連のコードを削除しました。コードベースをシンプルに保ち、メンテナンス性を向上させます。

--- oasis/services/markdown_processor.py
+++ oasis/services/markdown_processor.py
@@ -1,4 +1,3 @@
-
 import mistune
 import re
 
@@ -17,7 +16,7 @@ class MarkdownProcessor:
         )
 
     def convert_markdown_to_html(self, content: str) -> str:
-        content, link_previews = self._process_link_previews(content)
+        content, script_tags = self._preserve_script_tags(content)
         content, blockquotes = self._preserve_blockquotes(content)
         html_body = self._convert_to_html(content)
         if DEBUG : save_html_file(html_body, "_html_body_convert_to_html.html")
@@ -25,28 +24,15 @@ class MarkdownProcessor:
         if DEBUG : save_html_file(html_body, "_html_body_make_inline_code_bold.html")
         html_body = self._restore_blockquotes(html_body, blockquotes)
         if DEBUG : save_html_file(html_body, "_html_body_restore_blockquotes.html")
-        html_body = self._restore_link_previews(html_body, link_previews)
-        if DEBUG : save_html_file(html_body, "_html_body_restore_link_previews.html")
+        html_body = self._restore_script_tags(html_body, script_tags)
+        if DEBUG : save_html_file(html_body, "_html_body_restore_script_tags.html")
         return html_body
 
-    def _process_link_previews(self, content):
-        link_previews = {}
-        def replace_url_with_placeholder(match):
-            url = match.group(0)
-
-            # simple card
-            # preview_html = generate_site_card(url)
-
-            # iframe
-            preview_html = generate_iframely_embed(url)
-
-
-            placeholder = f"LINK_PREVIEW_PLACEHOLDER_{len(link_previews)}"
-            link_previews[placeholder] = preview_html
-            return f"{url}\n\n{placeholder}\n"
-
-        processed_text = re.sub(r'^https?://\S+$', replace_url_with_placeholder, content, flags=re.MULTILINE)
-        return processed_text, link_previews
+    def _preserve_script_tags(self, content):
+        script_tags = re.findall(r'<script.*?>.*?</script>', content, re.DOTALL)
+        for i, tag in enumerate(script_tags):
+            content = content.replace(tag, f'SCRIPT_TAG_PLACEHOLDER_{i}')
+        return content, script_tags
 
     def _preserve_blockquotes(self, content):
         blockquotes = re.findall(r'<blockquote.*?>.*?</blockquote>', content, re.DOTALL)
@@ -63,20 +49,12 @@ class MarkdownProcessor:
         return html_body
 
     def _make_inline_code_bold(self, html_body):
-        # code_blocks = re.findall(r'<pre><code.*?>.*?</code></pre>', html_body, re.DOTALL)
-        # for i, block in enumerate(code_blocks):
-        #     html_body = html_body.replace(block, f'CODE_BLOCK_PLACEHOLDER_{i}')
-
-        # html_body = re.sub(r'<code>(.*?)</code>', r'<strong>\1</strong>', html_body)
         html_body = re.sub(r'<code>([^<\n]+)</code>', r'<strong>\1</strong>', html_body)
-        # for i, block in enumerate(code_blocks):
-        #     html_body = html_body.replace(f'CODE_BLOCK_PLACEHOLDER_{i}', block)
-
         return html_body
 
-    def _restore_link_previews(self, html_body, link_previews):
-        for placeholder, preview_html in link_previews.items():
-            html_body = html_body.replace(placeholder, preview_html)
+    def _restore_script_tags(self, html_body, script_tags):
+        for i, tag in enumerate(script_tags):
+            html_body = html_body.replace(f'SCRIPT_TAG_PLACEHOLDER_{i}', tag)
         return html_body
 
 def read_markdown_file(file_path):
@@ -89,12 +67,10 @@ def save_html_file(html_content, file_path):
     print(f"HTMLが {file_path} に保存されました。")
 
 if __name__ == "__main__":
-
-    input_file = r"draft\ARC01\ARC01.md"  # 入力するマークダウンファイルの名前
+    input_file = r"draft\roomba01\README.md"  # 入力するマークダウンファイルの名前
     output_file = "output_mistune.html"  # 出力するHTMLファイルの名前
 
     markdown_content = read_markdown_file(input_file)
     processor = MarkdownProcessor()
     html_content = processor.convert_markdown_to_html(markdown_content)
     save_html_file(html_content, output_file)
-
